773515aaff030be30298799eabe37e951ff43431
Pranjal <nottletottle@gmail.com>
2024-11-12 22:34:05 +0545

    code folding enabled

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,7 +2,12 @@ vim.opt.tabstop = 2 -- number of visual spaces per TAB
 vim.opt.softtabstop = 2 -- number of spacesin tab when editing
 vim.opt.shiftwidth = 2 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
-
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 4
 -- UI config
 vim.opt.number = true -- show absolute number
 vim.opt.relativenumber = true -- add numbers to each line on the left side
