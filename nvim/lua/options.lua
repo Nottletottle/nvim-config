9c166eef6253a0b4f83580634c0f33d5cb120696
Pranjal <nottletottle@gmail.com>
2024-11-12 22:23:23 +0545

    indent-blankline added

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -28,8 +28,8 @@ vim.opt.incsearch = true -- search as characters are entered
 vim.opt.hlsearch = false -- do not highlight matches
 vim.opt.ignorecase = true -- ignore case in searches by default
 vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
-vim.opt.numberwidth = 2 -- Set number column width
-vim.opt.signcolumn = "yes:1" -- Make sign column 2 characters wide
+vim.opt.numberwidth = 1 -- Set number column width
+vim.opt.signcolumn = "yes:1" -- Make sign column 1 characters wide
 
 vim.opt.clipboard:remove("unnamedplus")
 -- Key mappings
